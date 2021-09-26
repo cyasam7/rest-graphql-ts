88ab28b3304e0cbae3767692c8bf1b72f1e8bb61
Alexander Serrano <cyasam7@gmail.com>
2021-09-26 18:26:39 -0500

    se agrego variables de entorno

--- api/src/config/enviroment.ts
+++ api/src/config/enviroment.ts
@@ -6,10 +6,22 @@ interface config {
     port: string;
     host: string;
     secret: string;
+    type_db: string;
+    host_db: string;
+    port_db: number;
+    username_db: string;
+    password_db: string;
+    database: string;
 }
 
 export const config: config = {
     port: String(process.env.PORT),
     host: String(process.env.HOST),
     secret: String(process.env.SECRET),
+    type_db: String(process.env.TYPE_DB),
+    host_db: String(process.env.HOST_DB),
+    port_db: parseInt(String(process.env.PORT_DB)),
+    username_db: String(process.env.USERNAME_DB),
+    password_db: String(process.env.PASSWORD_DB),
+    database: String(process.env.DATABASE),
 };
--- api/src/config/typeorm.ts
+++ api/src/config/typeorm.ts
@@ -1,16 +1,19 @@
 import { createConnection, useContainer } from "typeorm";
 import { entities } from "../apps";
 import { Container } from "typeorm-typedi-extensions";
+import { config } from "./enviroment";
+
+console.log(config);
 export async function connect() {
     try {
         useContainer(Container);
         await createConnection({
             type: "mysql",
-            host: "localhost",
+            host: config.host_db,
             port: 3306,
-            username: "root",
-            password: "",
-            database: "graphql",
+            username: config.username_db,
+            password: config.password_db,
+            database: config.database,
             entities,
             synchronize: true,
         });
